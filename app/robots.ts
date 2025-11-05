ab8ae219e1faa7f5ecc255b6ea929f6dbadf8b41
calorenzo21 <celorenzo2104@gmail.com>
2025-11-05 12:37:17 -0400

    add public url to robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://domemedia.com";
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.dome-media.com";
 
   return {
     rules: [
